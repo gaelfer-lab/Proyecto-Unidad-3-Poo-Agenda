ebbd4c6283bc3d63b850a8cb472b1fe96e85cfff
gaelfer-lab <gael05fer@gmail.com>
2026-04-03 15:52:21 -0700

    Cree la clase Contacto

---
+++ Contacto.java
@@ -0,0 +1,53 @@
+//Proyecto Agenda
+import java.util.ArrayList;
+
+public class Contacto extends Persona{
+    // Atributos de clase con valores por defecto
+    private String correo="Sin correo";
+    private ArrayList<Telefono> listaTelefonos;
+    // Constructor de la clase
+    public Contacto(String nombre, String apellido, char sexo, String alias, String correo) {
+        super(nombre, apellido, sexo, alias);
+        this.correo = correo;
+        this.listaTelefonos = new ArrayList<Telefono>();
+
+    }
+    // Metodos GET y SET
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public ArrayList<Telefono> getListaTelefonos() {
+        return listaTelefonos;
+    }
+    // Metodo para agregar un telefono a un contacto
+    public void agregarTelefono(Telefono tel){
+        this.listaTelefonos.add(tel);
+    }
+    //Metodo para eliminar un telefono de un contacto
+    public void eliminarTelefono(Telefono tel){
+        this.listaTelefonos.remove(tel);
+    }
+    //Metodo para devolver todos los telefonos de un tipo dado (M o F).
+    public ArrayList<Telefono> getTelefonosPorTipo(char tipo){
+        ArrayList<Telefono> resultado = new ArrayList<>();
+    char t = Character.toUpperCase(tipo);
+    for (Telefono tel : listaTelefonos) {
+        if (tel.getTipo() == t) resultado.add(tel);
+        }
+        return resultado;
+    }
+    @Override
+    public String toString() {        return "Contacto{" +
+                "nombre='" + getNombre() + '\'' +
+                ", apellido='" + getApellido() + '\'' +
+                ", sexo=" + getSexo() +
+                ", alias='" + getAlias() + '\'' +
+                ", correo='" + correo + '\'' +
+                ", listaTelefonos=" + listaTelefonos +
+                '}';
+    }
+    
+}
