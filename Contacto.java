05d9adf2334f08c8ad14aafb82c878f3b67a7192
gaelfer-lab <gael05fer@gmail.com>
2026-04-03 16:04:33 -0700

    Agrego contacto

--- Contacto.java
+++ Contacto.java
@@ -2,17 +2,16 @@
 import java.util.ArrayList;
 
 public class Contacto extends Persona{
-    // Atributos de clase con valores por defecto
+    // Atributos de clase con valores por defecto - Gael
     private String correo="Sin correo";
     private ArrayList<Telefono> listaTelefonos;
-    // Constructor de la clase
+    // Constructor de la clase - Gael
     public Contacto(String nombre, String apellido, char sexo, String alias, String correo) {
         super(nombre, apellido, sexo, alias);
         this.correo = correo;
         this.listaTelefonos = new ArrayList<Telefono>();
-
     }
-    // Metodos GET y SET
+    // Metodos GET y SET - Gael
     public String getCorreo() {
         return correo;
     }
@@ -22,15 +21,15 @@ public class Contacto extends Persona{
     public ArrayList<Telefono> getListaTelefonos() {
         return listaTelefonos;
     }
-    // Metodo para agregar un telefono a un contacto
+    // Metodo para agregar un telefono a un contacto - Gael
     public void agregarTelefono(Telefono tel){
         this.listaTelefonos.add(tel);
     }
-    //Metodo para eliminar un telefono de un contacto
+    //Metodo para eliminar un telefono de un contacto - Gael
     public void eliminarTelefono(Telefono tel){
         this.listaTelefonos.remove(tel);
     }
-    //Metodo para devolver todos los telefonos de un tipo dado (M o F).
+    //Metodo para devolver todos los telefonos de un tipo dado (M o F) - Gael
     public ArrayList<Telefono> getTelefonosPorTipo(char tipo){
         ArrayList<Telefono> resultado = new ArrayList<>();
     char t = Character.toUpperCase(tipo);
